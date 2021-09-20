b6e957b1c38111872845ccbb6bc42203bfd2e5e2
Muhammad Hassam <hassamshaikh94@gmail.com>
2021-09-20 10:16:14 +0500

    socket work update

--- CRS-frontend/src/App.js
+++ CRS-frontend/src/App.js
@@ -1,10 +1,17 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import Router from '../src/components/config/router';
+import socketIOClient from "socket.io-client";
 import url from "./baseurl/baseURL"
 
 function App() {
 
+  useEffect(() => {
+    var socket = socketIOClient(url);
+    socket.on('connect', function () {
+      console.log("connected")
+    });
+  }, [])
   return (
     <>
       <Router />
